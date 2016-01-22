88139a3f414849ab17633a781fc11539f2d7ef69
Sean Stahly <seanstahly@gmail.com>
2016-01-22 13:06:33 -0600

    finished HW2 part 1a

--- 408/HW2.rkt
+++ 408/HW2.rkt
@@ -1,1 +1,14 @@
 #lang racket
+(display "1. ")(newline)
+(define (reverse-digits n)
+  (define (reverse in out digitsMoved)
+    (if (= in 0)
+        out
+        (reverse (floor (/ in 10)) (+ (* out 10) (modulo in 10))  (+ digitsMoved 1))
+        )
+    )
+  (reverse n 0 0))
+(reverse-digits 456)
+(reverse-digits 123456789123456)
+(define (same-digits? a b)
+  (4))

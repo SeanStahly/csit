20b5c75a0b6dd8335874e28756a557bba240dfdb
Sean Stahly <seanstahly@gmail.com>
2016-01-27 13:03:01 -0600

    finished problem 1 part 2

--- 408/HW2.rkt
+++ 408/HW2.rkt
@@ -1,14 +1,27 @@
 #lang racket
 (display "1. ")(newline)
 (define (reverse-digits n)
-  (define (reverse in out digitsMoved)
+  (define (reverse in out )
     (if (= in 0)
         out
-        (reverse (floor (/ in 10)) (+ (* out 10) (modulo in 10))  (+ digitsMoved 1))
+        (reverse (floor (/ in 10)) (+ (* out 10) (modulo in 10)))
         )
     )
-  (reverse n 0 0))
-(reverse-digits 456)
-(reverse-digits 123456789123456)
+  (reverse n 0))
+(display "Testing reverse-digits 456 ")(reverse-digits 456)
+(display "Testing reverse-digits 123456789123456 ")(reverse-digits 123456789123456)
+(display "Testing reverse-digits 397164 ")(reverse-digits 397164)
+
 (define (same-digits? a b)
-  (4))
+  (define (adjust in out)
+    (if (= in 0)
+        out
+        (adjust (floor (/ in 10))(+ out (* 1 (expt 10 (modulo in 10)))))))
+  (if (= (adjust a 0) (adjust b 0))
+         #t
+         #f))
+(display "Testing same-digits 445, 544 ")(same-digits? 445 544)
+(display "Testing same-digits 133042, 420313 ")(same-digits? 133042 420313)
+(display "Testing same-digits 133043, 42013 ")(same-digits? 133042 42013)
+
+(display "2.")(newline)

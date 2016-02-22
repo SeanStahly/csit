bfdb837c68eacf29157154e74038314cfa14b3a2
Sean Stahly <seanstahly@gmail.com>
2016-02-22 13:12:04 -0600

    finished hw3?

--- 408/HW3.rkt
+++ 408/HW3.rkt
@@ -66,6 +66,59 @@
 
 (define (odd? n)
   ((complement even?) n))
-(even? 4)
-(odd? 9)
-(odd? 4)
+
+;b
+(define x 5)
+
+(define expression1 
+  (lambda (a b c) (+ (* a (square x)) (* b x) c)))
+(expression1 1 2 3)
+;(define expression2)
+(let ((a 1) (b 2) (c 3))
+  (+ (* a (square x)) (* b x) c))
+
+;c
+;let* required to set multiple lets sequentially
+(let* ((x 4)
+      (y (+ x 1)))
+  (+ x y))
+
+;d
+;24
+(display "Original")(newline)
+(let ((+ -) 
+      (- +) 
+      (* /) 
+      (/ *)) 
+  (* (/ (- (+ -6) (- 3 2 5)) 3) 2))
+(display "New let*")(newline)
+(let* ((a +)
+      (+ -) 
+      (- a)
+      (b *)
+      (* /) 
+      (/ b)) 
+  (* (/ (- (+ -6) (- 3 2 5)) 3) 2))
+
+;e
+(define (iffy test consequent alternative)
+  (cond ((eq? test #t) consequent)
+        ((eq? test #f) alternative)))
+
+(define (compareN p1 p2 n)
+  (iffy (= (p1 n) (p2 n))
+     #t
+     #f))
+(compareN square cube 2) 
+;Value: #f
+(compareN square cube 1) 
+;Value #t 
+(define compare-scN
+  (make-comparator square cube))
+(compare-scN 1)
+;Value: #t
+(test compare-scN 1 3) ;; tests compare-sc with 1, 2 and 3
+;Value: #f
+(define test-1-3N
+  (make-test 1 3)) ;; a test with 1, 2 and 3 as test values
+(test-1-3N compare-scN)

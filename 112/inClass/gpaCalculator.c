8abf712cd8ed218a383dc63d2a9af78ee8e2c273
Sean Stahly <seanstahly@gmail.com>
2017-02-07 10:20:13 -0600

    nested loops

--- 112/inClass/gpaCalculator.c
+++ 112/inClass/gpaCalculator.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
 int main() {
-  float test1, test2, test3;
-  printf("Please enter the first test score.\n");
-  scanf("%f", &test1);
-  printf("Please enter the second test score.\n");
-  scanf("%f", &test2);
-  printf("Please enter the third test score.\n");
-  scanf("%f", &test3);
 
-  float total = test1 + test2 + test3;
-  float gpa = total / 3;
+  int count = 0;
+  while (count < 5) {
+    char name[20];
+    printf("Please enter the name of the student.\n");
+    scanf("%s", name);
 
-  printf("Your test average is %.2f\n", gpa);
+    float total = 0;
+    int testCount =1;
 
-  if (gpa >= 70.00) {
-    printf("Congratulations! You received a passing grade!\n");
-  } else {
-    printf("Sorry. You did not receive a passing grade.\n");
+    while(testCount <= 3) {
+      float testScore = 0;
+      printf("Please enter the score for test %d.\n", testCount);
+      scanf("%f", &testScore);
+      total += testScore;
+      testCount += 1;
+    }
+
+    float gpa = total / 3;
+
+    printf("Your test average is %.2f\n", gpa);
+
+    if (gpa >= 70.00) {
+      printf("Congratulations %s! You received a passing grade!\n", name);
+    } else {
+      printf("Sorry %s. You did not receive a passing grade.\n", name);
+    }
+    count++;
   }
 
   return 0;
